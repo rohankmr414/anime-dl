e8c45622824c64d4e662bd31c68f71c798fb6f92
rohankmr414 <rohankmr414@gmail.com>
2019-12-12 12:27:50 +0530

    updated anime-dl.py

--- anime-dl.py
+++ anime-dl.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import os
+import os, asyncio
 from downloader_cli.download import Download
 
 keyword = input("Enter anime to search: ")
@@ -11,9 +11,17 @@ soup = BeautifulSoup(html_data.text,'html.parser')
 a_tags = soup.find_all(attrs={"class":"items"})
 
 l = a_tags[0].find_all("a")
-t = a_tags[0].get_text()
+t = a_tags[0].get_text().replace(" ", "").split("\n")
 
-print(t)
+
+for i in range(0, t.count('')):
+	t.remove('')
+
+count = 0
+for t_index in range(0, len(t), 2):
+	print(count, t[t_index], t[t_index+1])
+	count += 1
+
 
 links = []
 
@@ -27,10 +35,9 @@ i = int(input('Enter index no.: '))
 name = links[i].split("/")[-1]
 
 try:
-    os.mkdir(os.getcwd() + "/{}".format(name))
-    os.system("cd {}".format(name))
+	os.mkdir(os.getcwd() + "/{}".format(name))
 except Exception as e:
-    os.system("cd {}".format(name))
+	pass
 
 episodes = requests.get("https://www.gogoanime.io{}".format(links[i])).text
 soup2 = BeautifulSoup(episodes,'html.parser')
@@ -52,13 +59,16 @@ for k in range(1,e):
 
 #vidlinks = []
 
+def download_video(link, destination):
+	Download(link, destination).download()
+
 for ep_link in ep_links:
 	eppage = requests.get(ep_link)
 	soup3 = BeautifulSoup(eppage.text,'html.parser')
 	tags = soup3.find_all(attrs={"class":"anime_video_body_cate"})
-	
+
 	v = tags[0].find_all("a")
-	
+
 	garbages = []
 
 	for garbage in v:
@@ -68,7 +78,9 @@ for ep_link in ep_links:
 
 	dlpage = requests.get(garbages[-1])
 	soup4 = BeautifulSoup(dlpage.text,'html.parser')
-
+	
 	dllinks = soup4.find_all(attrs={"class": "mirror_link"})
 	f_link = dllinks[0].a["href"]
-	Download(f_link).download()
+	f_link = f_link.replace(" ", "%20")
+
+	download_video(f_link, name)
